fae563e5194f5cb9cfe6db7ade390392eaa5d8c3
mehtab <mehtabalam220@gmail.com>
2021-12-07 17:20:36 +0530

    add property image

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -25,7 +25,7 @@ mongodb_host = os.environ.get("NOSQL_HOST") or "localhost"
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
