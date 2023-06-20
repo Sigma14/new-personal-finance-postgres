f6d7c6de23e2b997da89680c2557e95cdc9d93db
Yash Khatri <mehtabalam220@gmail.com>
2023-06-20 17:18:01 +0530

    changed api url

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'finance.wsgi.application'
-stock_app_url = "http://portfolio_web_1"
+stock_app_url = "http://portfolio_web_1:8072"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
