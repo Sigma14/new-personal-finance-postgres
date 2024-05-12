426d0618bdb0e8b9587880f84e3b42724a68f4e5
Aashish Porwal <aashish.growing.it.100@gmail.com>
2024-05-12 15:52:24 +0530

    Resolved issue in adding goals and fund

--- finance/my_finance/views.py
+++ finance/my_finance/views.py
@@ -4090,6 +4090,7 @@ def goal_obj_save(request, goal_obj, user_name, fun_name=None):
     goal_date = request.POST['goal_date']
     account_name = request.POST['account_name']
     allocate_amount = 0
+    if(goal_date==""): goal_date = None
     # allocate_amount = request.POST['allocate_amount']
     account_obj = Account.objects.get(name=account_name)
     print("category_name=======>", category_name)
