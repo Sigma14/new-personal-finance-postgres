b57c009bdb1d5fb6135068261459a4ee487fee0b
Aashish Porwal <aashish.growing.it.100@gmail.com>
2024-06-25 12:02:16 +0530

    Made changes to helper.py

--- finance/my_finance/helper.py
+++ finance/my_finance/helper.py
@@ -157,20 +157,20 @@ def start_end_date(date_value, period):
 
 
 def get_period_date(start_date, period):
-    period_date = None
+
     if period == "Daily":
         period_date = start_date + relativedelta(days=1)
 
-    elif period == "Weekly":
+    if period == "Weekly":
         period_date = start_date + relativedelta(weeks=1)
 
-    elif period == "Monthly":
+    if period == "Monthly":
         period_date = start_date + relativedelta(months=1)
 
-    elif period == "Quarterly":
+    if period == "Quarterly":
         period_date = start_date + relativedelta(months=3)
 
-    elif period == "Yearly":
+    if period == "Yearly":
         period_date = start_date + relativedelta(years=1)
 
     return period_date
