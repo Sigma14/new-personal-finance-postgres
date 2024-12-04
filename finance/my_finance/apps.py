007485c891cdd1815783e11700039c56528c447a
tanzid64 <tanzid3@gmail.com>
2024-12-04 14:45:06 +0600

    clear branch

--- finance/my_finance/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyFinanceConfig(AppConfig):
-    name = 'my_finance'
--- finance/static/assets/js/loader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.querySelector('.my-loader');
-
-  loader.classList.add('my-loader-hidden');
-
-  loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader); 
-  })
-})
