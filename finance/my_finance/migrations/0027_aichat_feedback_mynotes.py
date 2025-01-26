d634a7ef4f0665841a847f08e0651306ef5ff604
tanzid64 <tanzid3@gmail.com>
2025-01-26 23:47:51 +0600

    fix: migration 0027

--- finance/my_finance/migrations/0027_aichat_feedback_mynotes.py
+++ finance/my_finance/migrations/0027_aichat_feedback_mynotes.py
@@ -15,16 +15,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='MyNotes',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('notes', models.TextField()),
-                ('added_on', models.DateField(default=datetime.date.today)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
             name='Feedback',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
